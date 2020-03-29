93e3e6f7c419f87a578ed1d1843de798b4f75b5a
Joyce Tijani <joytijani01@gmail.com>
2020-03-28 22:16:34 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Princess Joy on 3/28/20.
+//  Copyright © 2020 JoyTijani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
